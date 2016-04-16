b0c96d6312439fbdba779540c4e00f7574ecffdd
Carlos Santos <carlosricardosantos@gmail.com>
2016-04-16 20:48:28 +0100

    2 space indent + exit check

--- controllers/gpiotoggler.js
+++ controllers/gpiotoggler.js
@@ -6,38 +6,40 @@ module.exports = class GpioToggler {
 
     constructor() {
         
-        console.log("Setting up GpioToggler")
-        var Gpio = require('onoff').Gpio;
-        this.pin = new Gpio(constants.GPIO_PORT, 'out');
-        process.on('SIGINT', this.exit);
+      console.log("Setting up GpioToggler")
+      var Gpio = require('onoff').Gpio;
+      this.pin = new Gpio(constants.GPIO_PORT, 'out');
+      process.on('SIGINT', this.exit);
     }
 
     toggle(){
 
-        console.log("Toggling");
-        var previousState = this.pin.readSync();
-        var newState = previousState ^ 1;
-        this.pin.writeSync(newState);
-        return newState;
+      console.log("Toggling");
+      var previousState = this.pin.readSync();
+      var newState = previousState ^ 1;
+      this.pin.writeSync(newState);
+      return newState;
     }
 
     turnOn(){
 
-        this.pin.writeSync(1);
+      this.pin.writeSync(1);
     }
 
     turnOff(){
 
-        this.pin.writeSync(0);
+      this.pin.writeSync(0);
     }
 
     exit() {
 
-      this.pin.unexport();
+      if(this.pin){
+          this.pin.unexport();
+      }
       process.exit();
     }
 
     toString() {
-        return '(' + this.pin + ')';
+      return '(' + this.pin + ')';
     }
 }
