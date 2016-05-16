7d35c2912ae35ac5534887e4b84649f6bf678875
Carlos Santos <carlosricardosantos@gmail.com>
2016-05-16 14:54:48 +0100

    Added initial logging and Switched states as relay works the other way around

--- config/constants.js
+++ config/constants.js
@@ -3,4 +3,12 @@ module.exports = Object.freeze({
     SERVER_PORT: 3000,
     GPIO_PORT: 17,
     VERSION: 1,
+    STATUS : {
+    	ACTIVE   : 0,
+    	INACTIVE : 1
+    },
+    WRITE_STATE : {
+    	ON : "on",
+    	OFF : "off",
+    }
 });
--- controllers/gpiotoggler.js
+++ controllers/gpiotoggler.js
@@ -13,29 +13,45 @@ module.exports = class GpioToggler {
       }
       var Gpio = require('onoff').Gpio;
       this.pin = new Gpio(constants.GPIO_PORT, 'out');
-      this.turnOff(); // defaults to OFF
+      var weakthis = this;
+      this.turnOn(function(err){
+          console.info("Initial on/off cycle...");
+          weakthis.turnOff(function(err){
+            console.info("Strobelight booted up!");
+          }); // defaults to OFF at startup
+      });
+      
       process.on('SIGINT', this.exit);
     }
 
     toggle(){
 
       console.info("Toggling");
+      if(os.platform() != 'linux'){
+        return 1;
+      }
       var previousState = this.pin.readSync();
       var newState = previousState ^ 1;
       this.pin.writeSync(newState);
-      return newState;
+      return (newState ? constants.WRITE_STATE.OFF : constants.WRITE_STATE.ON);
     }
 
     turnOn(callback){
 
       console.info("Turning on");
-      this.pin.write(1, callback);
+      if(os.platform() != 'linux'){
+        return;
+      }
+      this.pin.write(constants.STATUS.ACTIVE, callback);
     }
 
     turnOff(callback){
 
       console.info("Turning off");
-      this.pin.write(0, callback);
+      if(os.platform() != 'linux'){
+        return;
+      }
+      this.pin.write(constants.STATUS.INACTIVE, callback);
     }
 
     exit() {
