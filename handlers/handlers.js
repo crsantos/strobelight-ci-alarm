17bbddaec901eab46d883300fcd3279b39e1938b
testark <testark@mailinator.com>
2016-05-18 09:55:57 +0100

    Forcibly kill Omxplayer...

--- handlers/handlers.js
+++ handlers/handlers.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const constants = require('../config/constants.js');
+var Exec = require('child_process').exec;
 var GpioToggler = require('../controllers/gpiotoggler.js')
 var Mathjs = require('mathjs');
 var Omxplayer = require('node-omxplayer');
@@ -12,6 +13,12 @@ var randomSound = function () {
   var name = 'sound-' + Mathjs.randomInt(constants.AUDIO_MAX_RANDOM) + '.mp3';
   return Path.join(constants.AUDIO_BASE_PATH, name);
 };
+
+var stopPlayer = function (player) {
+  player.quit();
+  // It's too bad that the previous call doesn't always terminate Omxplayer...
+  Exec('killall -9 omxplayer.bin', function () {});
+}
 
 module.exports = Object.freeze({
 
@@ -44,7 +51,7 @@ module.exports = Object.freeze({
 
             gpiToggler.turnOff(function(err){
               try {
-                player.quit();
+                stopPlayer(player);
               } catch (err) {
                 console.log("couldn't stop audio file");
                 console.log(err);
